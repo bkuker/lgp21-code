2d0a6711e7081b158b8ac4fbf7ac559ab87e6078
bkuker <bkuker@billkuker.com>
2026-04-26 17:45:35 -0400

    Change tracks, add size entry points

--- mandelbrot/mandelbrot.asm
+++ mandelbrot/mandelbrot.asm
@@ -1,59 +1,85 @@
+    .entry large
+
+
 ;Vertical range & size
-.2136:	iLow:	.dw -1.1 >> 4
-.1224:	iHigh:	.dw 1.1 >> 4
-.1229:	iStep:	.dw 0.1 >> 4
+.2236:	iLow:	.dw -1.1 >> 4
+.2224:	iHigh:	.dw 1.1 >> 4
+.2229:	iStep:	.dw 0.1 >> 4
 
 ;Horizontal range & size
-.1231:	rLow:	.dw -2.1 >> 4
-.1230:	rHigh:	.dw 0.52 >> 4
-.1235:	rStep:	.dw 0.040 >> 4
+.2231:	rLow:	.dw -2.1 >> 4
+.2230:	rHigh:	.dw 0.52 >> 4
+.2235:	rStep:	.dw 0.040 >> 4
 
 ;Maximum number of iterations
-.1233:	max:	.dw #28
+.2233:	max:	.dw #28
 
 ;Current position
-.1245:	iPos:	.dw 0
-.1262:	rPos:	.dw 0;
+.2245:	iPos:	.dw 0
+.2262:	rPos:	.dw 0;
 
 ;Iteration variables
-.1251:	itr:	.dw 0
-.1213:	x:	    .dw 0
-.1206:	xTemp:	.dw 0
-.1247:	xx:	    .dw 0
-.1225:	y:	    .dw 0
-.1218:	yy:	    .dw 0
+.2251:	itr:	.dw 0
+.2213:	x:	    .dw 0
+.2206:	xTemp:	.dw 0
+.2247:	xx:	    .dw 0
+.2225:	y:	    .dw 0
+.2218:	yy:	    .dw 0
 
 ;Constants for computation
-.1243:	dec:	.dw #2
-.1221:	twoF:	.dw 2.0 >> 4
-.1255:	fourF:	.dw 4.0 >> 4
+.2243:	dec:	.dw #2
+.2221:	twoF:	.dw 2.0 >> 4
+.2255:	fourF:	.dw 4.0 >> 4
 
 ;Constants for output
-.1226:	eol:	.dw "\n"
-.1252:	space:	.dw " "
+.2226:	eol:	.dw "\n"
+.2252:	space:	.dw " "
 
-;THESE ARE UNUSED, BUT SOME NICE SIZES
-;MIGHT ADD SIZE SELECTOR
+    .org 2115
+small:
+    ld iLowSmall
+    st iLow
+    ld iStepSmall
+    st iStep
+    ld rStepSmall
+    st rStep
+    jmp start
+medium:
+    ld iLowMed
+    st iLow
+    ld iStepMed
+    st iStep
+    ld rStepMed
+    st rStep
+    jmp start
+large:
+    ld iLowLarge
+    st iLow
+    ld iStepLarge
+    st iStep
+    ld rStepLarge
+    st rStep
+    jmp start
+
 ;Tiny 20 min
 ;Vertical range & size
-;iLowSmall:	.dw -1.2 >> 4
-;iStepSmall:	.dw 0.30 >> 4
-;rStepSmall:	.dw 0.120 >> 4
+iLowSmall:	.dw -1.2 >> 4
+iStepSmall:	.dw 0.30 >> 4
+rStepSmall:	.dw 0.120 >> 4
 
 ;Small 70 min
 ;Vertical range & size
-;iLowMed:	.dw -1.2 >> 4
-;iStepMed:	.dw 0.15 >> 4
-;rStepMed:	.dw 0.060 >> 4
+iLowMed:	.dw -1.2 >> 4
+iStepMed:	.dw 0.15 >> 4
+rStepMed:	.dw 0.060 >> 4
 
 ;Big 2.6 hours
 ;Vertical range & size
-;iLowLarge:	.dw -1.1 >> 4
-;iStepLarge:	.dw 0.1 >> 4
-;rStepLarge:	.dw 0.040 >> 4
+iLowLarge:	.dw -1.1 >> 4
+iStepLarge:	.dw 0.1 >> 4
+rStepLarge:	.dw 0.040 >> 4
 
-    .org 1000
-    .entry start
+    .org 2000
 
 start:                      ;Start a new fractal  
             ld  iLow        ;Reset iPos to iLow
@@ -72,7 +98,7 @@ newLine:                    ;Start a new line
 
             sub iHigh       ;If iPos > iHigh goto start
             jnt nextChar    ;else goto nextChar:
-            hlt             ; TODO jmp start
+            jmp done
 
 nextChar:                   ;Work out the next character
             ld  rPos        ;rPos += rStep
@@ -103,16 +129,16 @@ iterate:
             mulh x
             shl4
             st xx
-            jmp 1039
-.1039:
+            jmp 2039
+.2039:
             sub yy          ; ... - y*y
             add rPos        ; + x0
             st  xTemp
 
                             ; y := 2*x*y + y0
             ld twoF
-            jmp 1048
-.1048:
+            jmp 2048
+.2048:
             mulh x
             shl4
             mulh y
@@ -146,4 +172,7 @@ out:        ld itr
 in:         ld space
             pr6
             jmp nextChar
+done:
+    hlt
+    jmp start
 
